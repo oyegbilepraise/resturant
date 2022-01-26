a1fd7243f7d57a7aaca398bfb88a53ba0923a77e
praise <oluwadhammieh@gmail.com>
2022-01-26 16:06:31 +0100

    feat: Added Order and Cart C

---
+++ pages/cart.jsx
@@ -0,0 +1,62 @@
+import styles from "../styles/Cart.module.css";
+import Image from "next/image";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image src="/img/pizza.png" layout="fill" objectFit="cover" />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredients, spicy sause
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$19.90</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$39.80</span>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.09
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discout:</b>$0.09
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discout:</b>$0.09
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
